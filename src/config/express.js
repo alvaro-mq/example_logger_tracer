65f16ba2d29e3662ff373f830d9c24f219e7b59c
alvaro <almamani@agetic.gob.bo>
2020-08-25 00:00:40 -0400

    adicionando escritura de logs en archivos + response-time

--- src/config/express.js
+++ src/config/express.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const express = require('express');
+const responseTime = require('response-time');
+const pinoHttp = require('pino-http')
+const logger = require('../config/logger');
 const rTracer = require('cls-rtracer');
 
 const routes = require('../api/routes');
@@ -8,6 +11,11 @@ const routes = require('../api/routes');
 const app = express();
 
 app.use(express.json());
+
+app.use(pinoHttp({
+  logger
+}));
+app.use(responseTime());
 app.use(rTracer.expressMiddleware({ useHeader: true }))
 
 app.use('/v1', routes);
--- src/config/logger.js
+++ src/config/logger.js
@@ -1,16 +1,28 @@
 'use strict';
+const fs = require('fs')
 const pino = require('pino');
+const multistream = require('pino-multi-stream').multistream;
 const rTracer = require('cls-rtracer');
+
+const streams = [
+  {
+    stream: fs.createWriteStream('logs/combined.log')
+  },
+  {
+    level: 'error', stream: fs.createWriteStream('logs/error.log')
+  }
+];
 
 const options = {
   name: require('../../package.json').name,
-  prettyPrint: {
+ /*  prettyPrint: {
     levelFirst: true
-  },
+  }, */
   mixin: () => {
     return { requestId: rTracer.id() }
   }
 };
-const logger = pino(options);
+
+const logger = pino(options, multistream(streams, { dedupe: true }));
 
 module.exports = logger;
